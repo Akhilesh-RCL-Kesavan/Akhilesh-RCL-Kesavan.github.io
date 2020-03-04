1f7ed332536be49527eb2b339e9606fad6e7f589
Akhilesh-RCL-Kesavan <ak96.k7@gmail.com>
2020-03-04 22:16:13 +0530

    Create GROMACS_Quote.js

---
+++ GROMACS_Quote.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var quotes = fs.readFileSync('assets/Quotes.txt').toString().split("\n");
+
+# Counts total images
+totalQuotes = quotes.length
+
+# Get Quote
+getQuote = ->
+  # Gets random image from array
+  activeQuotes = quotes[Math.floor((Math.random() * totalQuotes))]
+
+  # Changes background of div
+  $('div').text(activeQuotes)
+
+getQuote()
+
+$('button').on 'click', (e) ->
+  e.preventDefault()
+
+  getQuote()
