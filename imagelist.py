d2a8075d08e8ce9db0964362234f360cb27a601a
Akhilesh Kesavan <ak96.k7@gmail.com>
2022-09-26 23:56:22 +0530

    Update imagelist.py

--- imagelist.py
+++ imagelist.py
@@ -1,1 +1,24 @@
 import os
+
+dir_path = './assets/EMBO/'
+
+# list to store files
+res = []
+
+# Iterate directory
+for path in os.listdir(dir_path):
+    # check if current path is a file
+    if os.path.isfile(os.path.join(dir_path, path)):
+        res.append(path)
+
+
+with open("out.txt", "w") as f:
+    for img in res:
+        f.write("\t<div class=\"mySlides fade\">\n\t\t<img src=\""+dir_path[1:]+img+"\" alt=\""+img[:-4]+">\n\t</div>")
+
+
+
+
+
+# "<img src=\""+dir_path[1:]+img+"\" alt=\""+img[:-4]+"\" loading=\"lazy\">"
+# "\t<div class=\"mySlides fade\">\n\t\t<img src=\""+dir_path[1:]+img+"\" alt=\""+img[:-4]+">\n\t</div>"
